50839b8b10a28ee21114648abcc3c4687a31aa82
Sven Moser <sven.mo99ser@gmail.com>
2022-02-11 16:43:34 +0100

    added cards

--- script/script.ts
+++ script/script.ts
@@ -1,46 +1,51 @@
-var weekdays: Array<menu> = [];
-class menu {
-    day: string;
-    preCourse: string;
-    mainCourse: string;
-    dessert: string;
-    price: number;
-    // image: string;
-    constructor(weekday: string, pre: string, main:string, dessert:string, price:number) {
-                this.day = weekday;
-                this.preCourse = pre;
-                this.mainCourse = main;
-                this.dessert = dessert;
-                this.price = price;
-                // this.image = image;
+var allAnimals: Array<animal> = [];
+class animal {
+    name: string;
+    gender: string;
+    size: string;
+    age: number;
+    vaccine: boolean;
+    image : string;
+    constructor(name: string, gender: string, size:string, age:number, vaccine: boolean,image : string) {
+                this.name = name;
+                this.gender = gender;
+                this.size = size;
+                this.age = age;
+                this.vaccine = vaccine;
+                this.image = image;
 
-                weekdays.push(this);
+                allAnimals.push(this);
     }
-    menuFunction() {
+    display() {
         return `<div class="col">
-          <div class="card" style="width: 18rem;">
-          <img src="..." class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-                </div>
+        <div class="card" style="width: 18rem;">
+        <img src="${this.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+<h5 class="card-title">${this.name}</h5>
+<p class="card-text">${this.gender}</p>
+<p class="card-text">${this.size}</p>
+<p class="card-text">${this.age}</p>
+<p class="card-text">${this.vaccine}</p>
+</div>
+</div>
         </div>`
     }
 }
 
 //Create Variables for each day, Variable definition not neccessary
-new menu ("Monday", "Tomato Soup", "Vegan Lasagne", "Chocolate Brownies", 20);
-new menu ("Tuesday", "Garlic Cream Soup", "Vegan Bolognese", "Panna Cotta", 22);
-new menu ("Wednesday", "Mixed Salad", "Vegan Curry", "Hazelnut Marzipan Cake", 18);
-new menu ("Thurday", "Carot-Ginger Soup", "Vegan Burger with French Fries", "Vegan Pancakes", 17);
-new menu ("Friday", "Mushroom Soup", "Tofu Masala", "Cherry Cake", 21);
-new menu ("Saturday", "Miso Soup", "Crunchy Pumpkin", "Vegan Cookies", 25);
-new menu ("Sunday", "Zuccini Soup", "Vegan Gyros", "Carrot Cake", 23);
+new animal ("Bacon", "Male", "30cm", 3, true,"https://cdn.pixabay.com/photo/2013/02/21/19/13/agriculture-84702_960_720.jpg");
+new animal ("Bacon", "Male", "30cm", 3, true,"https://cdn.pixabay.com/photo/2013/02/21/19/13/agriculture-84702_960_720.jpg");
+new animal ("Bacon", "Male", "30cm", 3, true,"https://cdn.pixabay.com/photo/2013/02/21/19/13/agriculture-84702_960_720.jpg");
+new animal ("Bacon", "Male", "30cm", 3, true,"https://cdn.pixabay.com/photo/2013/02/21/19/13/agriculture-84702_960_720.jpg");
+new animal ("Bacon", "Male", "30cm", 3, true,"https://cdn.pixabay.com/photo/2013/02/21/19/13/agriculture-84702_960_720.jpg");
+new animal ("Bacon", "Male", "30cm", 3, true,"https://cdn.pixabay.com/photo/2013/02/21/19/13/agriculture-84702_960_720.jpg");
+new animal ("Bacon", "Male", "30cm", 3, true,"https://cdn.pixabay.com/photo/2013/02/21/19/13/agriculture-84702_960_720.jpg");
+new animal ("Bacon", "Male", "30cm", 3, true,"https://cdn.pixabay.com/photo/2013/02/21/19/13/agriculture-84702_960_720.jpg");
+new animal ("Bacon", "Male", "30cm", 3, true,"https://cdn.pixabay.com/photo/2013/02/21/19/13/agriculture-84702_960_720.jpg");
+
 
 var selectorEl = document.getElementsByClassName("cards")[0] as HTMLElement;
 
-for (let val of weekdays) {
-    selectorEl.innerHTML += val.menuFunction();
+for (let val of allAnimals) {
+    selectorEl.innerHTML += val.display();
 }
